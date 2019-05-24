ec84a90a25018cf4bfccca23537181987f025a56
EldyBlue <eldera@live.fr>
2019-05-24 23:54:03 +0200

    classes auth + request (wok)

---
+++ www/core/Request.class.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace MVC\Core;
+
+
+class Request {
+
+    private $getArgs;
+    private $postArgs;
+
+    /**
+     * Request constructor.
+     */
+    public function __construct() {
+        $this->getArgs = $_GET;
+        $this->postArgs = $_POST;
+    }
+
+    /**
+     * @param $name
+     * @return mixed|string
+     */
+    public function get($name) {
+        if (!empty($this->getArgs)) {
+            foreach ($this->getArgs as $key => $value) {
+                if ($key == $name) {
+                    return $value;
+                }
+            }
+        } else if (!empty($this->postArgs)) {
+            foreach ($this->postArgs as $key => $value) {
+                if ($key == $name) {
+                    return $value;
+                }
+            }
+        }
+        return null;
+    }
+
+
+
+}
