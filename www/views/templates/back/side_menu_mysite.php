f4e5d4761be87b192552449db1a4b2f5ce5b2d91
Himyo <himyo97@gmail.com>
2019-06-26 11:47:34 +0200

    wip

--- www/views/templates/back/side_menu_mysite.php
+++ www/views/templates/back/side_menu_mysite.php
@@ -3,7 +3,6 @@
         <input type="text" class="search" placeholder=" Rechercher" />
         <button type="button" name="button"><i class="fas fa-search"></i></button>
     </div>
-    <a href="/page"><p><i class="fas fa-chart-line"></i> Gestion des pages</p></a>
     <a href="/list/cards"><p><i class="fas fa-chart-line"></i> Gestion des Cards</p></a>
     <a href="/list/users"><p><i class="fas fa-chart-line"></i> Gestion des Users</p></a>
     <a href="/list/articles"><p><i class="fas fa-chart-line"></i> Gestion des Articles</p></a>
