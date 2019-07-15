cb7a70be7e76be58d353dc608569f9ca285f68ee
Himyo <himyo97@gmail.com>
2019-07-15 15:13:54 +0200

    final commit

--- www/views/register.view.php
+++ www/views/register.view.php
@@ -1,5 +1,22 @@
 <div id="container-user" class="container">
     <div class="content">
-        <?php $this->addComponent("formRegister", "");?>
+        <?php 
+            if(isset($_GET['error'])) {
+                $error = $_GET['error'];
+                switch($error) {
+                    case 'emailnotunique':
+                        echo '<div class="background-color: #d9534f; border-color: #d43f3a;"><p>Cette email est deja pris</p></div>';
+                    break;
+                    case 'pwdmatching':
+                        echo '<div class="background-color: #d9534f; border-color: #d43f3a;"><p>Les mots de passe ne correspondent pas</p></div>';
+                    break;
+                    case 'missingfield':
+                        echo '<div class="background-color: #d9534f; border-color: #d43f3a;"><p>Veuillez remplir les champs principaux</p></div>';
+                    break;
+                }
+
+            }
+            $this->addComponent("formRegister", "");
+        ?>
     </div>
 </div
