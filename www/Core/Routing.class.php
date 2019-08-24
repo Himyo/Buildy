0a6f95a66e48f8ca06d20e30dbc17d6f2e7ba915
Himyo <himyo97@gmail.com>
2019-08-24 20:15:44 +0200

    redone 404 page because it was removed for no good reason ffs

--- www/Core/Routing.class.php
+++ www/Core/Routing.class.php
@@ -31,53 +31,6 @@ class Routing{
         ];
     }
 
-    public static function getParametrableRoute($slug) {
-	    $routes = yaml_parse_file(self::$routeFile);
-	    $routesSlugs = array_keys($routes);
-	    $slug = trim($slug, '/');
-        $slugToCompare = explode("/", $slug)[0];
-        $matchedSlug = "";
-	   for($i = 0 ; $i < sizeof($routesSlugs) ; ++$i) {
-           $routeSlug = trim($routesSlugs[$i], '/');
-	       $routeSlug = explode('/', $routeSlug)[0];
-	       if($slugToCompare == $routeSlug) {
-	           $matchedSlug = $routesSlugs[$i];
-	           break;
-           }
-       }
-	   if($matchedSlug) {
-           $slugParameters = explode('/', $slug);
-           unset($slugParameters[0]);
-           $slugParametersNumber = sizeof($slugParameters);
-           $matchedSlugParametersNumber = sizeof(explode('!', $matchedSlug)) -1;
-           $hasOptional = sizeof(explode('?', $matchedSlug)) -1 > 0;
-
-           $valid = $slugParametersNumber == $matchedSlugParametersNumber;
-           $validWithOptional = ($slugParametersNumber >= $matchedSlugParametersNumber) && $hasOptional;
-
-           if($valid || $validWithOptional) {
-               $controller = $routes[$matchedSlug]["controller"] == "" ? $slugParameters[1] : $routes[$matchedSlug]["controller"];
-
-               $controller = ucfirst($controller)."Controller";
-               $action = $routes[$matchedSlug]["action"]."Action";
-               $method = $routes[$matchedSlug]["method"];
-
-               $_POST[$controller][$action] = $slugParameters;
-               return
-                   [
-                       "controller" => $controller,
-                       "action" => $action,
-                       "method" => $method,
-                   ];
-           }
-       }
-        return [
-            "controller" => 'PagesController',
-            "action" => 'notFoundAction',
-            "method" => 'post'
-        ];
-    }
-
     public static function getCrudRoute($slug) {
         $slug = strtolower($slug);
         $routes = yaml_parse_file(self::$routeFile);
